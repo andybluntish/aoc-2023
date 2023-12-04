063110f3e9b557c1d2e1b410222828c795fa3db8
Andy Stanford-Bluntish <andy@bluntish.net>
2023-12-04 10:30:20 +1030

    Day 3 cleanup

--- 03_Gear_Ratios/day_3.rb
+++ 03_Gear_Ratios/day_3.rb
@@ -1,40 +1,32 @@
 #!/usr/bin/env ruby -w
+require "debug"
 
 module Day3
   module Part1
     def self.run(input)
-      Day3.find_parts_numbers(input).flatten.compact.map(&:number).sum
+      Day3.find_parts_numbers(input).flatten.compact.map(&:value).sum
     end
   end
 
   module Part2
     def self.run(input)
+      lines, width, height = Day3.parse_input(input)
       parts_numbers = Day3.find_parts_numbers(input)
       gear_ratios = []
-      grid = input.split("\n")
-      width = grid.first.length - 1
-      height = grid.length - 1
 
-      grid.each_with_index do |line, y|
+      lines.each_with_index do |line, y|
         line.chars.each_with_index do |char, x|
           next unless char.match?(/\*/)
 
-          start = x - 1
-          finish = x + 1
-          left = [0, start].max
-          right = [width, finish].min
-
-          bounds = []
-          bounds << [left, y] unless left == 0
-          bounds << [right, y] unless right == width
-          (left..right).each { |x| bounds << [x, y - 1] } if y > 0
-          (left..right).each { |x| bounds << [x, y + 1] } if y < height
-
-          numbers = bounds
-            .select { |x, y| grid[y][x].match?(/[0-9]/) }
-            .map { |x, y| parts_numbers[y].filter { |n| x >= n.start && x <= n.finish } }
-            .flatten.uniq
-          gear_ratios << numbers.map(&:number).inject(:*) if numbers.length == 2
+          start = x
+          finish = x
+          Location.new(char, start, finish, y).tap do |loc|
+            numbers = loc.perimeter(width, height)
+              .select { |x, y| lines[y][x].match?(/[0-9]/) }
+              .map { |x, y| parts_numbers[y].filter { |n| x >= n.start && x <= n.finish } }
+              .flatten.uniq
+            gear_ratios << numbers.map(&:value).inject(:*) if numbers.length == 2
+          end
         end
       end
       gear_ratios.sum
@@ -42,60 +34,67 @@ module Day3
   end
 
   class << self
+    def parse_input(input)
+      lines = input.split("\n")
+      width = lines.first.length - 1
+      height = lines.length - 1
+      [lines, width, height]
+    end
+
     def find_parts_numbers(input)
       numbers = []
+      lines, width, height = Day3.parse_input(input)
 
-      grid = input.split("\n")
-      width = grid.first.length - 1
-      height = grid.length - 1
-
-      grid.each_with_index do |line, y|
+      lines.each_with_index do |line, y|
         number_buffer = ""
         line.chars.each_with_index do |char, x|
-          if char.match?(/[0-9]/)
-            number_buffer += char
-          end
+          number_buffer += char if char.match?(/[0-9]/)
 
-          if char.match?(/[^0-9]/) || x == line.length - 1
+          if char.match?(/[^0-9]/) || x == width
             if number_buffer.length > 0
               finish = x - 1
-              start = x - number_buffer.length
-              left = [0, start - 1].max
-              right = [width, finish + 1].min
-              bounds = []
-              bounds << [left, y] unless left == 0
-              bounds << [right, y] unless right == width
-              (left..right).each { |x| bounds << [x, y - 1] } if y > 0
-              (left..right).each { |x| bounds << [x, y + 1] } if y < height
-
-              bounds.each do |point|
-                if grid[point[1]][point[0]].match?(/[^0-9\.]/)
-                  numbers[y] ||= []
-                  numbers[y] << PartsNumber.new(number_buffer.to_i, start, y)
-                  break
+              start = finish - (number_buffer.length - 1)
+              Location.new(number_buffer.to_i, start, finish, y).tap do |loc|
+                loc.perimeter(width, height).each do |px, py|
+                  if lines[py][px].match?(/[^0-9\.]/)
+                    numbers[y] ||= []
+                    numbers[y] << loc
+                    break
+                  end
                 end
               end
-
               number_buffer = ""
             end
           end
         end
       end
-
       numbers
     end
   end
 
   private
 
-  class PartsNumber
-    attr_reader :number, :start, :finish, :y
+  class Location
+    attr_reader :value, :start, :finish, :y
 
-    def initialize(number, start, y)
-      @number = number
+    def initialize(value, start, finish, y)
+      @value = value
       @start = start
-      @finish = start + number.to_s.length - 1
+      @finish = finish
       @y = y
+    end
+
+    def perimeter(width, height)
+      @perimeter ||= begin
+        left = [0, start - 1].max
+        right = [width, finish + 1].min
+        bounds = []
+        bounds << [left, y] unless left == 0
+        bounds << [right, y] unless right == width
+        (left..right).each { |x| bounds << [x, y - 1] } if y > 0
+        (left..right).each { |x| bounds << [x, y + 1] } if y < height
+        bounds
+      end
     end
   end
 end
