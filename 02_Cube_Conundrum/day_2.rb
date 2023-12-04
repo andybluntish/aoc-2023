30765b933cee8fda92ac425192eb6057275702f7
Andy Stanford-Bluntish <andy@bluntish.net>
2023-12-04 13:14:09 +1030

    Day 2 cleanup

--- 02_Cube_Conundrum/day_2.rb
+++ 02_Cube_Conundrum/day_2.rb
@@ -25,47 +25,30 @@ module Day2
       _, id, rounds = *input.match(/Game (\d+):\s(.*)/)
 
       @id = id.to_i
-      @rounds = rounds.split(";").map do |round|
-        results = {}
-        round.split(",").each do |result|
-          _, score, colour = *result.match(/(\d+)\s(\w+)/)
-          results[colour.to_sym] = score.to_i
-        end
-        results
+      @results = rounds.split(/[;,]/).map do |round|
+        _, score, colour = *round.match(/(\d+)\s(\w+)/)
+        [colour.to_sym, score.to_i]
       end
     end
 
-    attr_reader :id, :rounds
+    attr_reader :id
 
     def possible?
-      possible = true
-      @rounds.each do |round|
-        round.each do |colour, score|
-          possible = score <= MAX_SCORES[colour]
-          break unless possible
-        end
-        break unless possible
+      @results.all? do |colour, score|
+        score <= MAX_SCORES[colour]
       end
-      possible
     end
 
     def power
-      counts = {red: 0, green: 0, blue: 0}
-      @rounds.map do |round|
-        round.map do |colour, score|
-          counts[colour] = score if score > counts[colour]
-        end
-      end
-      counts.values.inject(:*)
+      @results.each_with_object(Hash.new(0)) do |round, scores|
+        colour, score = round
+        scores[colour] = score if score > scores[colour]
+      end.values.inject(:*)
     end
 
     private
 
-    MAX_SCORES = {
-      red: 12,
-      green: 13,
-      blue: 14
-    }
+    MAX_SCORES = {red: 12, green: 13, blue: 14}
   end
 end
 
